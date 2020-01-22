b731859cb1775bb4caa729b3f07a5f210703a608
deskout <45195426+deskout@users.noreply.github.com>
2020-01-22 16:26:36 +0100

    Start
    Primer commit con la primera versión del código

---
+++ MSE_Web/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>MSE Club</title>
+        <link rel="shortcut icon" href="img/favicon/favicon.ico" type="image/x-icon"/>
+        <link rel="stylesheet" type="text/css" href="css/mse.css">
+        <script type="text/javascript" src="js/jquery-3.4.1.js"></script>
+        <script type="text/javascript" src="js/mse.js"></script>
+    </head>
+    <body>
+        <div class="header">
+            <div class="logo">
+                <img src="img/logo/MortSubite-150x150.png" alt="Mort Subite eSports" title="Mort Subite eSports" />
+            </div>
+            <div class="opciones">
+                <ul class="listaOpciones">
+                    <li><a href="#" class="optHeader">Inicio</a></li>
+                    <li><a href="#" class="optHeader">Store</a></li>
+                    <li><a href="#" class="optHeader">Equipos</a></li>
+                    <li><a href="#" class="optHeader">Media</a></li>
+                    <li><a href="#" class="optHeader">Noticias</a></li>                    
+                </ul>                
+            </div>
+            <div class="rrssheader">
+                <img src="img/logo/MortSubite-150x150.png" alt="Mort Subite eSports" title="Mort Subite eSports" />
+                <img src="img/logo/MortSubite-150x150.png" alt="Mort Subite eSports" title="Mort Subite eSports" />
+                <img src="img/logo/MortSubite-150x150.png" alt="Mort Subite eSports" title="Mort Subite eSports" />
+                <img src="img/logo/MortSubite-150x150.png" alt="Mort Subite eSports" title="Mort Subite eSports" />
+                <img src="img/logo/MortSubite-150x150.png" alt="Mort Subite eSports" title="Mort Subite eSports" />
+            </div>
+        </div>
+        <div class="content">
+            <div class="test"></div>
+        </div>
+        <div class="sponsor">
+            
+        </div>
+        <div class="footer">
+            
+        </div>
+    </body>
+</html>
