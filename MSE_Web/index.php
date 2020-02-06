f7183a7f12230a7d72f90c00577ffbb9ce2cd98c
deskout <>
2020-02-06 16:01:25 +0100

    Segundo avance

--- MSE_Web/index.php
+++ MSE_Web/index.php
@@ -15,34 +15,139 @@ and open the template in the editor.
     </head>
     <body>
         <div class="header">
-            <div class="logo">
-                <img src="img/logo/MortSubite-150x150.png" alt="Mort Subite eSports" title="Mort Subite eSports" />
+            <div class="upSeparatorBarBlack">
+                <div class="logo">
+                    <img src="img/logo/MortSubite-150x150.png" alt="Mort Subite eSports" title="Mort Subite eSports" />
+                </div>
+                <div class="rrssheader">
+                    <ul class="rrssIMGs">
+                        <li><a href="yt"><img src="img/logo/MortSubite-150x150.png" alt="Mort Subite eSports" title="Mort Subite eSports" /></a></li>
+                        <li><a href="ig"><img src="img/logo/MortSubite-150x150.png" alt="Mort Subite eSports" title="Mort Subite eSports" /></a></li>
+                        <li><a href="https://www.twitch.tv/mortsubitetv"><img src="img/logo/MortSubite-150x150.png" alt="Twitch MSE" title="Twitch MSE" /></a></li>
+                        <li><a href="https://twitter.com/mse_club?lang=es"><img src="img/logo/MortSubite-150x150.png" alt="Twitter MSE" title="Twitter MSE" /></a></li>
+                    </ul>
+                </div>
             </div>
-            <div class="opciones">
-                <ul class="listaOpciones">
-                    <li><a href="#" class="optHeader">Inicio</a></li>
-                    <li><a href="#" class="optHeader">Store</a></li>
-                    <li><a href="#" class="optHeader">Equipos</a></li>
-                    <li><a href="#" class="optHeader">Media</a></li>
-                    <li><a href="#" class="optHeader">Noticias</a></li>                    
-                </ul>                
-            </div>
-            <div class="rrssheader">
-                <img src="img/logo/MortSubite-150x150.png" alt="Mort Subite eSports" title="Mort Subite eSports" />
-                <img src="img/logo/MortSubite-150x150.png" alt="Mort Subite eSports" title="Mort Subite eSports" />
-                <img src="img/logo/MortSubite-150x150.png" alt="Mort Subite eSports" title="Mort Subite eSports" />
-                <img src="img/logo/MortSubite-150x150.png" alt="Mort Subite eSports" title="Mort Subite eSports" />
-                <img src="img/logo/MortSubite-150x150.png" alt="Mort Subite eSports" title="Mort Subite eSports" />
-            </div>
+            <div class="upSeparatorBarYellow">
+                <div class="opciones">
+                    <ul class="listaOpciones">
+                        <li><a href="pages/noticias.php" class="optHeader">NOTICIAS</a></li>
+                        <li><a href="#" class="optHeader">TIENDA</a></li>
+                        <li><a href="#" class="optHeader">EQUIPOS</a></li>
+                        <li><a href="#" class="optHeader">SPONSORS</a></li>
+                        <li><a href="#" class="optHeader">CONTACTO</a></li>                    
+                    </ul>                
+                </div>
+            </div> 
         </div>
+        
         <div class="content">
-            <div class="test"></div>
-        </div>
-        <div class="sponsor">
+        	<div class="slider">
+        		<!-- <img src="img/test.png" /> -->
+        	</div>
+        	
+        	<div class="sponsors">
+            	<div class="sponsor">
+            		<div class="titleSponsor"><p>PATROCINADOR</p></div>
+            		<div class="contentSponsor">�PUEDES SER TU!</div>
+            	</div>
+            	<div class="sponsor">
+            		<div class="titleSponsor"><p>SPONSORS</p></div>
+            		<div class="contentSponsor">
+            			<img src="" alt="Cerro Graphics" title="Cerro Graphics" />
+            		</div>
+            	</div>
+            	<div class="sponsor">
+            		<div class="titleSponsor"><p>COLABORADORES</p></div>
+            		<div class="contentSponsor">
+            			<img src="" alt="Cerro Graphics" title="Cerro Graphics" />
+            			<img src="" alt="Cerro Graphics" title="Cerro Graphics" />
+            			<img src="" alt="Cerro Graphics" title="Cerro Graphics" />
+            		</div>
+            	</div>
+            </div>
             
+            <div class="noticias">
+            	<div class="noticia">
+            		<img src="" alt="Cerro Graphics" title="Cerro Graphics" />
+            	</div>
+            	<div class="noticia">
+            		<img src="" alt="Cerro Graphics" title="Cerro Graphics" />
+            	</div>            		
+            	<div class="noticia">
+            		<img src="" alt="Cerro Graphics" title="Cerro Graphics" />
+            	</div>
+            </div>
+            
+            <div class="nextMatches">
+            	<div class="matchesTitle"></div>
+            	<div class="match">
+            		<div class="matchHour">
+            			<p>23/01</p>
+            			<p>20:00</p>
+            		</div>
+            		<div class="matchTeam">MSE CLUB</div>
+            		<div class="matchLogo">LG</div>
+            		<div class="matchVS">VS</div>
+            		<div class="matchLogo">LG</div>
+            		<div class="matchTeam">FAZE CLAN</div>
+            		<div class="matchTwitch">VER DIRECTO</div>            		
+            	</div>
+            	<div class="match">
+            		<div class="matchHour">
+            			<p>23/01</p>
+            			<p>20:00</p>
+            		</div>
+            		<div class="matchTeam">MSE CLUB</div>
+            		<div class="matchLogo">LG</div>
+            		<div class="matchVS">VS</div>
+            		<div class="matchLogo">LG</div>
+            		<div class="matchTeam">FAZE CLAN</div>
+            		<div class="matchTwitch">VER DIRECTO</div>            		
+            	</div>
+            	<div class="match">
+            		<div class="matchHour">
+            			<p>23/01</p>
+            			<p>20:00</p>
+            		</div>
+            		<div class="matchTeam">MSE CLUB</div>
+            		<div class="matchLogo">LG</div>
+            		<div class="matchVS">VS</div>
+            		<div class="matchLogo">LG</div>
+            		<div class="matchTeam">FAZE CLAN</div>
+            		<div class="matchTwitch">VER DIRECTO</div>            		
+            	</div>
+            </div>
+            
+            <!-- <div class="tienda">
+            
+            </div>
+            
+            <div class="aladina">
+            
+            </div> -->
         </div>
+        
         <div class="footer">
-            
+            <div class="goMSE">#GoMSE</div>
+            <div class="separatorFooter"></div>
+            <div class="linksFooter">
+                <div class="legales">
+                    <ul>
+                        <li><a href="aviso" class="linkFooter">Aviso legal</a> | </li>
+                        <li><a href="cookies" class="linkFooter">Uso de cookies</a> | </li>
+                        <li><a href="terminos" class="linkFooter">Términos y condiciones</a> | </li>
+                        <li><a href="privacidad" class="linkFooter">Política de privacidad</a></li>
+                    </ul>
+                </div>
+                <div class="copyright">
+                    <img src="img/favicon/favicon-32x32.png" alt="Mort Subite eSports" title="Mort Subite eSports" />
+                    <span>Copyright &#169; 2020.</span>
+                    <button class="anclaTop">
+                        <span class="icoTop"></span>
+                    </button>
+                </div>                
+            </div>
         </div>
     </body>
 </html>
