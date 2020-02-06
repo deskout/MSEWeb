66a62b858ab53f1fb4aad8972d8f344f21b7f22c
deskout <>
2020-02-06 16:03:06 +0100

    Sub paginas añadidas

---
+++ MSE_Web/pages/contacto.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>MSE Club</title>
+        <link rel="shortcut icon" href="img/favicon/favicon.ico" type="image/x-icon"/>
+        <link rel="stylesheet" type="text/css" href="../css/mse.css">
+        <script type="text/javascript" src="js/jquery-3.4.1.js"></script>
+        <script type="text/javascript" src="js/mse.js"></script>
+    </head>
+    <body>
+        <div class="header">
+            <div class="upSeparatorBarBlack">
+                <div class="logo">
+                    <img src="img/logo/MortSubite-150x150.png" alt="Mort Subite eSports" title="Mort Subite eSports" />
+                </div>
+                <div class="rrssheader">
+                    <ul class="rrssIMGs">
+                        <li><a href="yt"><img src="img/logo/MortSubite-150x150.png" alt="Mort Subite eSports" title="Mort Subite eSports" /></a></li>
+                        <li><a href="ig"><img src="img/logo/MortSubite-150x150.png" alt="Mort Subite eSports" title="Mort Subite eSports" /></a></li>
+                        <li><a href="https://www.twitch.tv/mortsubitetv"><img src="img/logo/MortSubite-150x150.png" alt="Twitch MSE" title="Twitch MSE" /></a></li>
+                        <li><a href="https://twitter.com/mse_club?lang=es"><img src="img/logo/MortSubite-150x150.png" alt="Twitter MSE" title="Twitter MSE" /></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="upSeparatorBarYellow">
+                <div class="opciones">
+                    <ul class="listaOpciones">
+                        <li><a href="#" class="optHeader">NOTICIAS</a></li>
+                        <li><a href="#" class="optHeader">TIENDA</a></li>
+                        <li><a href="#" class="optHeader">EQUIPOS</a></li>
+                        <li><a href="#" class="optHeader">SPONSORS</a></li>
+                        <li><a href="#" class="optHeader">CONTACTO</a></li>                    
+                    </ul>                
+                </div>
+            </div> 
+        </div>
+        
+        <div class="content">
+
+        </div>
+        
+        <div class="footer">
+            <div class="goMSE">#GoMSE</div>
+            <div class="separatorFooter"></div>
+            <div class="linksFooter">
+                <div class="legales">
+                    <ul>
+                        <li><a href="aviso" class="linkFooter">Aviso legal</a> | </li>
+                        <li><a href="cookies" class="linkFooter">Uso de cookies</a> | </li>
+                        <li><a href="terminos" class="linkFooter">Términos y condiciones</a> | </li>
+                        <li><a href="privacidad" class="linkFooter">Política de privacidad</a></li>
+                    </ul>
+                </div>
+                <div class="copyright">
+                    <img src="img/favicon/favicon-32x32.png" alt="Mort Subite eSports" title="Mort Subite eSports" />
+                    <span>Copyright &#169; 2020.</span>
+                    <button class="anclaTop">
+                        <span class="icoTop"></span>
+                    </button>
+                </div>                
+            </div>
+        </div>
+    </body>
+</html>
